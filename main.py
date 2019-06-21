a28c0be17eebb4a53c2111cf54a741121962e505
Steven kessler <stvnksslr@gmail.com>
2019-06-20 21:04:06 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load Env Varriables
+load_dotenv()
+
+description = '''A Dominions 5 Bot for keeping track of games and turn state'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+bot.run(os.getenv("TOKEN"))
