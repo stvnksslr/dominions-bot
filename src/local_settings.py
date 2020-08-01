bb00212bbcb24c563429b0bf38f1443a4fc6d000
Steven Kessler <stvnksslr@gmail.com>
2020-08-01 14:46:17 -0700

    chore(ci fixes):

--- src/local_settings.py
+++ src/local_settings.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PLUGINS = ["machine.plugins.builtin.help.HelpPlugin", "plugins.dominions.TurnStatus"]
+PLUGINS = ["machine.plugins.builtin.help.HelpPlugin",
+           "plugins.dominions.TurnStatus",
+           "plugins.grog.Grog"]
 
 # Should the HTTP server be enabled?
 DISABLE_HTTP = True
