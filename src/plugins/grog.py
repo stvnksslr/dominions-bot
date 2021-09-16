3e845a68a60e79e714285f58fa2b626799ad66f4
Steven Kessler <stvnksslr@gmail.com>
2021-09-16 15:52:59 -0400

    feat(valheim players): added check to show players on our valheim server

--- src/plugins/grog.py
+++ src/plugins/grog.py
@@ -1,6 +1,7 @@
 from random import choice
 from machine.plugins.base import MachineBasePlugin
 from machine.plugins.decorators import listen_to
+from httpx import get
 
 grog_response_list = [
     "grog",
@@ -18,3 +19,11 @@ class Grog(MachineBasePlugin):
     def grog_responder(self, msg):
         random_grog = choice(grog_response_list)
         msg.say(text=random_grog)
+
+
+class ValheimServer(MachineBasePlugin):
+    @respond_to(r"vikings")
+    def status(self, msg):
+        status = get("167.172.8.35:32418/status.json").json
+        players = status.get("players")
+        msg.say(text=f"{len(players)} online")
